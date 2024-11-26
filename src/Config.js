4ff3b935dba24fd69811a95e222e4c02f10656df
LLLL Colonq <llll@colonq>
2024-11-25 23:34:34 -0500

    Config update

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,5 @@
 export const mode = globalThis.mode;
 export const apiServer = globalThis.apiServer;
+export const secureApiServer = globalThis.secureApiServer;
 export const clientID = globalThis.clientID;
 export const authRedirectURL = globalThis.authRedirectURL;
